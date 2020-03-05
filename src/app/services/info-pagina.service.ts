d3bd5a10de7e660b7262a9bedb316fe5ccf0d318
Mauricio Diaz <dwdmmadm2001@gmail.com>
2020-03-04 22:39:05 -0600

    Imagenes e información del about cargadas

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,15 +9,28 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){ 
+    
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
-    })
-  }
+    });
+   }
+
+   private cargarEquipo() {
+     //Leer el archivo JSON
+    this.http.get('https://angular-portafolio-1b70c.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      this.equipo = resp;
+    });
+   }
 }
