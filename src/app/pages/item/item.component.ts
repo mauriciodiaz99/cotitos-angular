2e53cf32f54039d54d378d9f49969291d4228d7e
Mauricio Diaz <dwdmmadm2001@gmail.com>
2020-03-05 21:00:33 -0600

    Pagina de busqueda y proceso de carga terminado

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -12,17 +12,18 @@ export class ItemComponent implements OnInit {
 
 
   producto: ProductoDescripcion;
+  productoid: string;
 
   constructor( private route: ActivatedRoute, public productoService: ProductosService ) { }
 
   ngOnInit() {
     this.route.params
     .subscribe( parametros => {
-      console.log(parametros['id']);
+      
       this.productoService.getProducto(parametros['id'])
       .subscribe( (producto: ProductoDescripcion) => {
+        this.productoid = parametros['id'];
         this.producto = producto;
-        console.log(producto);
       })
     })
   }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,20 +9,56 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) { 
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-portafolio-1b70c.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
-      this.productos = resp;
-      this.cargando = false;
+
+    return new Promise( (resolve, reject) =>{
+      
+      this.http.get('https://angular-portafolio-1b70c.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      });
+
     });
+
   }
 
   getProducto(id: string){
     return this.http.get(`https://angular-portafolio-1b70c.firebaseio.com/productos/${id}.json`)
+  }
+
+  buscarProducto(termino: string){
+
+    if( this.productos.length === 0 ){
+      //cargar productos
+      this.cargarProductos().then( ()=> {
+        //ejecutar despues de tener los productos
+        //Aplicar filtro
+        this.filtrarProductos( termino );
+      });
+    }else{
+      //aplicar filtro
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos( termino: string ){
+    console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if(prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ){
+        this.productosFiltrado.push( prod );
+      }
+    });
   }
 }
